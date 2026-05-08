d85fd1b5f068cea6f63b129445106facb7fc41a6
joshpersonal016-cell <josh.personal016@gmail.com>
2026-05-08 17:16:25 +0800

    Update customer.ts

--- ui/Customer_Onboarding_Form/src/types/customer.ts
+++ ui/Customer_Onboarding_Form/src/types/customer.ts
@@ -4,6 +4,7 @@ export interface Customer {
     lastName: string
     email: string
     phoneNumber: string
+    signatureUrl?: string
     createdAt?: string
     updatedAt?: string
 }
@@ -13,4 +14,24 @@ export interface CreateCustomerDto {
     lastName: string
     email: string
     phoneNumber: string
+    signature?: Blob | null
+}
+
+type CustomerForm = {
+  firstName: string
+  lastName: string
+  email: string
+  phoneNumber: string
+
+  signatureUrl: string | null
+  signatureBlob: Blob | null
+}
+
+export type Props = {
+  visible: boolean
+  onClose: () => void
+  onSubmit?: () => void
+  formData: CustomerForm
+  setFormData: React.Dispatch<React.SetStateAction<CustomerForm>>
+  readOnly?: boolean
 }

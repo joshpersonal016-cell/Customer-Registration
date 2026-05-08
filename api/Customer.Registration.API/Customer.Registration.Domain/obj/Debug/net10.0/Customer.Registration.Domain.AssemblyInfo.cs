159d9aa295305f0e6c091e344a721586d493c80e
Joshua <joshua.aguilar@racami.com>
2026-05-08 18:38:57 +0800

    Push v2

--- api/Customer.Registration.API/Customer.Registration.Domain/obj/Debug/net10.0/Customer.Registration.Domain.AssemblyInfo.cs
+++ api/Customer.Registration.API/Customer.Registration.Domain/obj/Debug/net10.0/Customer.Registration.Domain.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Customer.Registration.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d85fd1b5f068cea6f63b129445106facb7fc41a6")]
 [assembly: System.Reflection.AssemblyProductAttribute("Customer.Registration.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Customer.Registration.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
